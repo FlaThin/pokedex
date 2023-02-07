1d6c8c1e8a8bd6b0e85bf6a3e1e16093efdc29bc
Flathin <flathinsees@gmail.com>
2023-02-06 21:38:27 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Wellcome to Pokémon World</h1>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.tsx",
+    "./components/**/*.tsx"
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
